4e64a4244de2a21e57aa61c3a71b2f48e069eb9e
MatiasArzamendia <arzamendia.matias99@gmail.com>
2022-09-12 17:45:54 -0300

    haciendo el navbar responsive

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -2,12 +2,19 @@ import './NavBar.css';
 import logo from '../../assets/images/logo.jpg';
 import CartWidget from '../cartWidget/CartWidget';
 import { NavLink, Link } from 'react-router-dom';
-import { AiOutlineLogin, AiOutlineUser } from 'react-icons/ai';
+import {
+    AiOutlineLogin,
+    AiOutlineUser,
+    AiOutlineBars,
+    AiOutlineClose,
+} from 'react-icons/ai';
 import { cartContextProvider } from '../../context/CartContextProvider';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 const NavBar = () => {
     const { navigateTo, dataForm } = useContext(cartContextProvider);
+
+    const [bar, setBar] = useState(false);
 
     const activeStyle = {
         color: '#000',
@@ -16,6 +23,11 @@ const NavBar = () => {
 
     const style = {
         fontSize: '22px',
+    };
+
+    const barStyle = {
+        fontSize: '25px',
+        cursor: 'pointer',
     };
 
     const handleStyle = ({ isActive }) => (isActive ? activeStyle : undefined);
@@ -28,65 +40,106 @@ const NavBar = () => {
                         <img src={logo} alt="Logo de sonrisa" />
                     </Link>
                 </div>
-                <ul className="navBar">
-                    <NavLink to="/" className="link" style={handleStyle}>
-                        Inicio
-                    </NavLink>
-                    <NavLink to="/belleza" className="link" style={handleStyle}>
-                        Belleza
-                    </NavLink>
-                    <NavLink
-                        to="/electrodomesticos"
-                        className="link"
-                        style={handleStyle}
-                    >
-                        Electrodomésticos
-                    </NavLink>
-                    <NavLink
-                        to="/indumentaria"
-                        className="link"
-                        style={handleStyle}
-                    >
-                        Indumentaria
-                    </NavLink>
-                    <NavLink
-                        to="/tecnologia"
-                        className="link"
-                        style={handleStyle}
-                    >
-                        Tecnología
-                    </NavLink>
-                    <NavLink to="/muebles" className="link" style={handleStyle}>
-                        Muebles
-                    </NavLink>
+                <div className="barsContainer">
+                    {bar ? (
+                        <button onClick={() => setBar(false)}>
+                            <AiOutlineClose style={barStyle} />
+                        </button>
+                    ) : (
+                        <button onClick={() => setBar(true)}>
+                            <AiOutlineBars style={barStyle} />
+                        </button>
+                    )}
+                </div>
+                <ul className={`navBar ${bar ? 'showMenu' : 'hideMenu'}`}>
+                    <li>
+                        <NavLink to="/" className="link" style={handleStyle}>
+                            Inicio
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/belleza"
+                            className="link"
+                            style={handleStyle}
+                        >
+                            Belleza
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/electrodomesticos"
+                            className="link"
+                            style={handleStyle}
+                        >
+                            Electrodomésticos
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/indumentaria"
+                            className="link"
+                            style={handleStyle}
+                        >
+                            Indumentaria
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/tecnologia"
+                            className="link"
+                            style={handleStyle}
+                        >
+                            Tecnología
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/muebles"
+                            className="link"
+                            style={handleStyle}
+                        >
+                            Muebles
+                        </NavLink>
+                    </li>
                 </ul>
                 <ul className="navBarIcons">
                     {navigateTo ? (
-                        <NavLink
-                            to="/dashboard"
-                            className="link"
-                            style={handleStyle}
-                        >
-                            <div>
-                                <AiOutlineUser style={style} />
-                                <span>{dataForm.name}</span>
-                            </div>
-                        </NavLink>
+                        <li>
+                            <NavLink
+                                to="/dashboard"
+                                className="link"
+                                style={handleStyle}
+                            >
+                                <div>
+                                    <AiOutlineUser style={style} />
+                                    <span>{dataForm.name}</span>
+                                </div>
+                            </NavLink>
+                        </li>
                     ) : (
+                        <li>
+                            <NavLink
+                                to="/login"
+                                className="link"
+                                style={handleStyle}
+                            >
+                                <div>
+                                    <AiOutlineLogin style={style} />
+                                    <span>Iniciar sesión</span>
+                                </div>
+                            </NavLink>
+                        </li>
+                    )}
+                    <li>
                         <NavLink
-                            to="/login"
+                            to="/cart"
                             className="link"
                             style={handleStyle}
                         >
-                            <div>
-                                <AiOutlineLogin style={style} />
-                                <span>Iniciar sesión</span>
-                            </div>
+                            <CartWidget />
                         </NavLink>
-                    )}
-                    <NavLink to="/cart" className="link" style={handleStyle}>
-                        <CartWidget />
-                    </NavLink>
+                    </li>
                 </ul>
             </nav>
         </header>
